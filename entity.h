511db068dad6c549cf88f3a82229c73219e70023
TobyPacio44 <ppaciowski@gmail.com>
2025-06-06 04:22:56 +0200

    add files

---
+++ entity.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+
+namespace structures {
+
+    struct Entity {
+        std::string name;
+        int health = 100;
+        int baseArmor = 1;
+        int baseDamage = 1;
+        int mana = 0;
+        int posx = 0;
+        int posy = 0;
+
+        virtual void takeDamage(int damage) {
+            damage -= baseArmor;
+            if (damage > 0) {
+                health -= damage;
+            }
+            else {
+                health -= 1;
+            }
+        }
+
+        virtual bool isAlive() const {
+            return health > 0;
+        }
+    };
+
+}
